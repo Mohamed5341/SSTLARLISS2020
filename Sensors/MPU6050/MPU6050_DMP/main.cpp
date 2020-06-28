b6ed944276cf5107f3fbc86f92407987bad23517
Mohamed Ashraf <ashrafmohamed534@gmail.com>
2020-06-28 12:47:09 +0200

    add newly modified MPU6050 library

---
+++ Sensors/MPU6050/MPU6050_DMP/main.cpp
@@ -0,0 +1,22 @@
+#include "mbed.h"
+#include "MPULib/MyMPU6050.h"
+
+MyMPU6050 mpu(p28,p27,p10);
+
+int main()
+{
+    float angle;
+    
+    mpu.setup();
+
+    while (true) {
+        mpu.loop();
+
+        angle = mpu.get_angle()*180/M_PI;
+
+        printf("YPR\t%d.%d\n",(int)angle,(int)(abs(angle - (int)angle) * 100));
+
+        ThisThread::sleep_for(500ms);
+    }
+}
+
